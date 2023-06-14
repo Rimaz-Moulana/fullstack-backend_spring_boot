babdc775f25513fea9e9cdf3dd927ae59df4cf51
RimazMoulana <ahmedrimaz99@gmail.com>
2023-06-14 23:50:21 +0530

    fullstack-backend 2023/6/14 at 23:50

--- src/main/java/com/codewithrimaz/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewithrimaz/fullstackbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.codewithrimaz.fullstackbackend.controller;
 
+import com.codewithrimaz.fullstackbackend.exception.UserNotFoundException;
 import com.codewithrimaz.fullstackbackend.model.User;
 import com.codewithrimaz.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,22 @@ public class UserController {
     List<User> getAllUsers(){
         return userRepository.findAll();
     }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user->{
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
 
 }
